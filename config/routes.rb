22913fc54cce3d274d372f2f6d8475a67b71268d
shuheeeei <shuheeeei.kawashima@gmail.com>
2018-10-20 21:41:35 +0900

    create post index

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,9 @@
 Rails.application.routes.draw do
   devise_for :users
   root  'home#index'
-
   get 'home/index' => "home#index"
+
+  resources :meigens
 
   resources :users, only: [:edit, :update]
 
