e32e7d05ed030c10aac785285570272afb4e8b5c
shuheeeei <shuheeeei.kawashima@gmail.com>
2018-10-21 19:03:17 +0900

    Edit view and association

--- app/models/meigen.rb
+++ app/models/meigen.rb
@@ -1,5 +1,11 @@
 class Meigen < ApplicationRecord
   belongs_to :user
+  has_many :likes, dependent: :destroy
+
   mount_uploader :image, ImageUploader
   validates :content, presence: true
+
+  def like_user(id)
+    likes.find_by(user_id: id)
+  end
 end
