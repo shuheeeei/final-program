049ab6b597ea8eb3cd25e092f8e9541738b1ecc7
shuheeeei <shuheeeei.kawashima@gmail.com>
2018-10-29 21:12:51 +0900

    create my-page

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,14 @@
 Rails.application.routes.draw do
   devise_for :users
   root  'home#index'
+  get 'users/:id/likes' => 'likes#show'
 
   resources :meigens do
     resources :likes, only: [:create, :destroy]
     resources :comments, only: [:create, :destroy]
   end
 
-  resources :users, only: [:edit, :update]
+  resources :users, only: [:show, :edit, :update]
+
 
 end
