98f0c0a37390a5f99b0bb716968d4bf1df5302e9
shuheeeei <shuheeeei.kawashima@gmail.com>
2018-11-10 15:02:21 +0900

    revise bucket

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -13,5 +13,5 @@ CarrierWave.configure do |config|
   }
 
   config.fog_directory  = 'tech-exp-final-app'
-  config.asset_host = 'https://s3-ap-northeast-1.amazonaws.com/test-the-upload'
+  config.asset_host = 'https://s3-ap-northeast-1.amazonaws.com/tech-exp-final-app'
 end
