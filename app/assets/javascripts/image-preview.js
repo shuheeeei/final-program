3ec24bc07bf176a76bea7f11377782227476f3f9
shuheeeei <shuheeeei.kawashima@gmail.com>
2018-11-17 13:59:04 +0900

    画像再投稿時プレビュー機能の実装

---
+++ app/assets/javascripts/image-preview.js
@@ -0,0 +1,27 @@
+$(function(){
+  $('form').on('change', 'input[type="file"]', function(e) {
+    var file = e.target.files[0],
+        reader = new FileReader(),
+        $preview = $(".meigen-edit-container__content__left-content__preview");
+
+    if(file.type.indexOf("image") < 0){
+      return false;
+    }
+
+
+    reader.onload = (function(file) {
+      return function(e) {
+
+        $preview.empty();
+
+        $preview.append($('<img>').attr({
+                  src: e.target.result,
+                  class: "meigen-edit-container__content__left-content__preview",
+                  title: file.name
+              }));
+      };
+    })(file);
+
+    reader.readAsDataURL(file);
+  });
+});
