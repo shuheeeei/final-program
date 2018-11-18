7d23f1c7aa5f1159f2acf22d02d845938231d9c1
shuheeeei <shuheeeei.kawashima@gmail.com>
2018-11-18 14:05:09 +0900

    サイドメニュー追加

--- app/assets/javascripts/move-decoration.js
+++ app/assets/javascripts/move-decoration.js
@@ -7,5 +7,33 @@ $(function(){
     $('body,html').animate({scrollTop:position}, speed, 'swing');
     return false;
   });
+
+
+  $('#js__sideMenuBtn').on('click', function () {
+    $('.side-menu').animate({width: 'toggle'}, 300);
+  });
+
+  $('.exp-list-item1').on("click", function() {
+    $('.exp-list-content1').slideToggle(400);
+    var mark1 = $('.exp-list-item1 span').text();
+    if (mark1 == "+") {
+      $('.exp-list-item1 span').text("-");
+    };
+    if (mark1 == "-") {
+      $('.exp-list-item1 span').text("+");
+    };
+  });
+
+  $('.exp-list-item2').on("click", function() {
+    $('.exp-list-content2').slideToggle(400);
+    var mark2 = $('.exp-list-item2 span').text();
+    if (mark2 == "+") {
+      $('.exp-list-item2 span').text("-");
+    };
+    if (mark2 == "-") {
+      $('.exp-list-item2 span').text("+");
+    };
+  });
+
 });
 
