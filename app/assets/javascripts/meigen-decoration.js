b2ac6c9aba6b89491fe72e9daf55fb380d46bf1e
shuheeeei <shuheeeei.kawashima@gmail.com>
2018-11-25 18:39:14 +0900

    投稿文字数の表示上限機能の修正
    jQueryによる実装に変更
    投稿に改行が含まれていても対応可能に

--- app/assets/javascripts/meigen-decoration.js
+++ app/assets/javascripts/meigen-decoration.js
@@ -8,4 +8,26 @@ $(function(){
     $(this).css("transform", `rotate(${value}deg)`);
   });
 
+
+  $('.meigen-container__content__right-content__meigen').each(function() {
+    var $target = $(this);
+    var html = $target.html();
+    var $clone = $target.clone();
+    $clone
+      .css({
+        display: 'none',
+        position : 'absolute',
+        overflow : 'visible'
+      })
+      .width($target.width())
+      .height('auto');
+    $target.after($clone);
+    while((html.length > 0) && ($clone.height() > $target.height())) {
+      html = html.substr(0, html.length - 1);
+      $clone.html(html + '...');
+    }
+    $target.html($clone.html());
+    $clone.remove();
+  });
+
 })
