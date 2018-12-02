d7d4c74092cb3335ceb66c074d350643b5426195
shuheeeei <shuheeeei.kawashima@gmail.com>
2018-12-02 18:41:22 +0900

    viewへの反映

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,7 +1,7 @@
 class UsersController < ApplicationController
   def show
     user = User.find(params[:id])
-    @meigens = user.meigens.where(user_id: current_user.id).order("created_at DESC")
+    @meigens = user.meigens.where(user_id: current_user.id).page(params[:page]).per(10).order("created_at DESC")
   end
 
 end
